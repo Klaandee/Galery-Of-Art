3b5c77ae1c28281bcb4b2a25afd2a1d6f6a04554
Klande <jhonasemece@gmail.com>
2024-03-29 06:02:36 -0300

    edit

--- .netlify/functions-internal/ssr/chunks/galery_ve7iJYpB.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/galery_fLUWVahk.mjs').then(n => n.g);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/generic_DIjbalnl.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_981WQ_7Z.mjs');
-
-export { page };
